756434451b00e6b753d5f2c75cb164a5cfe37616
ardiandaffa <ardiandaffa@icloud.com>
2020-05-12 09:18:23 +0700

    updt

--- app/src/main/java/com/example/dicodjavv/MainActivity.java
+++ app/src/main/java/com/example/dicodjavv/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.dicodjavv;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -25,13 +28,43 @@ public class MainActivity extends AppCompatActivity {
         showReyclerList();
     }
 
-    public boolean onCreateOptionMenu(Menu menu){
-        getMenuInflater().inflate(R.);
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        setMode(item.getItemId());
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setMode(int SelectedMode) {
+        switch (SelectedMode){
+            case R.id.action_cardView:
+                break;
+            case R.id.action_grid:
+                showRecyclerGrid();
+                getSupportActionBar().setTitle("Mode Grid");
+                break;
+            case R.id.action_list:
+                showReyclerList();
+                getSupportActionBar().setTitle("Mode Title");
+                break;
+        }
     }
 
     private void showReyclerList(){
         rvHeroes.setLayoutManager(new LinearLayoutManager(this));
         ListHeroAdapter listheroadapter = new ListHeroAdapter(list);
         rvHeroes.setAdapter(listheroadapter);
+    }
+
+    private void showRecyclerGrid(){
+        rvHeroes.setLayoutManager(new GridLayoutManager(this,2 ));
+        GridHeroAdapter gridheroadapter = new GridHeroAdapter(list);
+        rvHeroes.setAdapter(gridheroadapter);
     }
 }
